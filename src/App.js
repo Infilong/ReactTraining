70d9e0f8c28623408a317f4339c10e1fb031b789
infilong <ytxqfe@gmail.com>
2022-08-30 21:49:19 +0800

    class example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React from "react";
 import Header from "./components/Header";
-function App() {
+//function App() {
   // const name = "Brad";
   // const x = true;
   // return (
@@ -8,12 +9,10 @@ function App() {
   //     <h2>Hello {x ? name : "No"}</h2>
   //   </div>
   // );
-
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
+//}
+class App extends React.Component{
+  render() {
+    return <h1>Git Tests</h1>
+  }
 }
-
 export default App;
