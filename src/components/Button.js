996f54e93b6f45bd5789f73d6aef83e66836cb0b
infilong <ytxqfe@gmail.com>
2022-08-31 21:05:47 +0800

    Button click function and log

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,23 @@
-export const Button = ({ color, text }) => {
+import PropTypes from "prop-types";
+const Button = ({ color, text, onClick }) => {
   return (
-    <button style={{ backgroundColor: color }} className="btn">
+    <button
+      style={{ backgroundColor: color }}
+      className="btn"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
 };
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Button is clicked")
+  }
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add"/>
-      <Button color="red" text="Delete"/>
-      <Button color="black" text="Save"/>
+      <Button text="Add" onClick={onClick}/>
+      <Button color="red" text="Delete" onClick={onClick}/>
+      <Button color="black" text="Save" />
     </header>
   );
 };
