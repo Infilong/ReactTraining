28f00a315c0fd8b8ebe2d2070addfd68b9ba87fe
infilong <ytxqfe@gmail.com>
2022-08-31 21:24:13 +0800

    Created a list with map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
   return (
-    <div className="container">
-      <Header title={"Header propTypes string example"}/>
-    </div>
-  )
-}
-
+    <>
+      <div className="container">
+        <Header title={"Header propTypes string example"} />
+      </div>
+      <div className="container">
+        <Tasks />
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,32 @@
+const tasks = [
+  {
+    id: 1,
+    text: "Test 1",
+    day: "Jan 1st at 5:00pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Test 2",
+    day: "Jan 2nd at 5:00pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Test 3",
+    day: "Jan 3rd at 5:00pm",
+    reminder: false,
+  },
+];
+
+const Tasks = () => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
