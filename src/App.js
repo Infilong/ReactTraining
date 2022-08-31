3a06798488ab76f8e39b561eed256f4a312dbba2
infilong <ytxqfe@gmail.com>
2022-08-31 21:34:12 +0800

    Moved data from Tasks.js to App.js as Global state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Test 1",
+      day: "Jan 1st at 5:00pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Test 2",
+      day: "Jan 2nd at 5:00pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Test 3",
+      day: "Jan 3rd at 5:00pm",
+      reminder: false,
+    },
+  ]);
   return (
     <>
       <div className="container">
         <Header title={"Header propTypes string example"} />
       </div>
       <div className="container">
-        <Tasks />
+        <Tasks tasks={tasks}/>
       </div>
     </>
   );
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,4 @@
-import { useState } from "react";
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Test 1",
-      day: "Jan 1st at 5:00pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Test 2",
-      day: "Jan 2nd at 5:00pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Test 3",
-      day: "Jan 3rd at 5:00pm",
-      reminder: false,
-    },
-  ]);
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
